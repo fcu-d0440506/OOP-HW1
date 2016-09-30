901b4270c38e9ba9a1b43432833b71878dd910b8
user <user@FD06>
2016-09-30 09:22:11 +0800

    second use

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		system.out.println("Hello World");
+		system.out.println("Hello oop");
 	}
 
 }

c840760eed54a5cddae2948bdfbf3a1a7939dd37
er860511 <er860511@DESKTOP-CPRQNVT>
2016-10-03 21:04:55 +0800

    homework first

---
+++ src/homework/first.java
@@ -0,0 +1,13 @@
+package homework;
+
+public class first {
+
+	public static void main(String[] args) {
+			String lyrics="Let it go! Let it go! Cannot hold it back anymore";
+				lyrics=lyrics.replaceAll("it","her");
+				lyrics=lyrics.replaceAll("Cannot","Can't");
+				System.out.println(lyrics);
+
+	}
+
+}
